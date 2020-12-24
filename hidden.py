8d8602dabeba96bc4140cd72cc619de82a1a3369
Subramaniam <sampathkumar.subramaniam@sap.com>
2020-12-25 00:32:01 +0100

    integrated tweets

---
+++ hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "3qOldVIoEcRO1c3w7v7IrmX7P",
+            "consumer_secret": "TjRqn3GGafR4y1QeZ0XnxwG0JtGzmg1fftXwE8qj55MS4P5qDw",
+            "token_key": "1340006465210363905-cHqkJ31CUsaC7EPysJ62jLf1XVFqBi",
+            "token_secret": "a3Q0yCoNgSnKgqfc9ck1EilBOfFncgdBqcweegEHf2xXC"}
