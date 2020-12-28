d2194755f31f0a6814852adda1ac0f4017e85bf0
Subramaniam <sampathkumar.subramaniam@sap.com>
2020-12-28 10:13:37 +0100

    added method for publishing to pubsub

--- gcp_services/retrieve_web_credentials.py
+++ gcp_services/retrieve_web_credentials.py
@@ -1,6 +1,17 @@
 from google.cloud import secretmanager
+from google.cloud import pubsub_v1
+import json
 
 PROJECT_ID = "analog-sum-299523"
+TOPIC_ID = "jonasa-web-wrong-credentials"
+
+
+def publish_msg(msg):
+    publisher = pubsub_v1.PublisherClient()
+    topic_path = publisher.topic_path(PROJECT_ID, TOPIC_ID)
+    msg = msg.encode("utf-8")
+    future = publisher.publish(topic_path, msg, origin="jonasa-cloud", username="jonasa")
+    print(future.result())
 
 
 def access_secret_version(secret_id, version_id="latest"):
@@ -8,9 +19,6 @@ def access_secret_version(secret_id, version_id="latest"):
     name = f"projects/{PROJECT_ID}/secrets/{secret_id}/versions/{version_id}"
     response = client.access_secret_version(name=name)
     return response.payload.data.decode('UTF-8')
-
-
-import json
 
 
 def get_credentials():
